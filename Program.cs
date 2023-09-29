aba8feb9d245b95c57b93fa9367eb2f83d6c23a2
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-09-29 08:40:32 -0300

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,12 +7,6 @@ public class Program{
 
         Pokedex pokedex = new Pokedex();
         pokedex.MostrarMenu();
-
-        //Alguns exemplos de pokemon são -> Pikachu, gengar, squirtle, charmander, etc.
-        //Professor, eu tentei colocar a funcionalidade de apagar elementos selecionados pelo usuário em lista, entretanto eu não consegui, embora tenha tentado de diversas formas.
-        //Por mais que caisse na condicional de "apagar" ele sempre voltava. Depois caso seja possível gostaria de que o sr pudesse me explicar o pq disso acontecer..
-
-
         }
     }
 }
